b38cf6c0586b6fb75f803760cf75c7465b605a51
fangurl <vinitishrivastava3@gmail.com>
2021-05-31 15:44:23 +0530

    scaffolding the project

---
+++ pages/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            various categorues
+        
+        </div>
+    )
+}
+
+export default Categories
---
+++ pages/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Checkout = () => {
+    return (
+        <div>
+            checkyourwayout
+        </div>
+    )
+}
+
+export default Checkout
